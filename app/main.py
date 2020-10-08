347d9a999578481223026014360d06f09692ab9e
Bryant Crocker <Crocker456@gmail.com>
2020-10-08 01:02:10 -0400

    modified app

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 from datetime import datetime
 from fastapi import FastAPI
+import numpy as np 
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return { "time": datetime.now() }
+    return { "message": 'hello world'}
+
+@app.get("/random_numbers")
+def random_numbers():
+    return { "random numbers": list(np.random.normal(0,1, 100))}
